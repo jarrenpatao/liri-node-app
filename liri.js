68d9cf840d6c28044b59ef3a2eb7b62c39c5db5a
jarrenpatao <jarrenpatao@gmail.com>
2018-12-06 20:17:57 -0800

    removed switch case and went back to conditional check for response

--- liri.js
+++ liri.js
@@ -1,95 +1,116 @@
-var result = require("dotenv").config();
-var fs = require('fs');
-var inquirer = require('inquirer');
-var keys = require("./keys");
+// add code to read and set any environment variables with the dotenv package:
+require("dotenv").config();
+
+var moment = require("moment");
+var axios = require("axios");
+var keys = require('./keys.js');
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 var bandsintown = keys.bandsintown;
 var omdbKey = keys.omdbKey;
-var moment = require('moment');
-var axios = require('axios');
-
-
-function bandsInTown(artist) {
-  var url = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=" + bandsintown.id;
-
-  // console.log(`Searching for ${artist} concerts...\nURL: ${url}\n`);
-
-  axios.get(url)
-  .then(function (response) {
-  console.log(JSON.stringify(response.config.url));
-
-    var data = response.data;
-    data.forEach(element => {
-      console.log(`Name: ${element.venue.name} \nLocation: ${element.venue.city}, ${element.venue.country} \nDate: ${moment(response.datetime).format("MM/DD/YYYY")}\n\n`);
-    });
-  })
-
-  .catch(function (error) {
-    console.log(error);
-  });
-  
-}
-
-function spotifySearch(song) {
-  spotify
-    .search({
-      type: 'track',
-      query: song
-    })
-    .then(function (response) {
-            
-      var data = response.tracks.items;
-      var artist = JSON.stringify(response.artist);
-      console.log(JSON.stringify(response.tracks.items, null, 2));
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
-}
-
-function omdb(movie) {
-  var url = "http://www.omdbapi.com/?t=" + query + "&apikey=" + omdbKey.id;
-
-  axios.get(url)
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-}
-
-var input = process.argv[2];
+var fs = require("fs");
+var command = process.argv[2];
 var query = process.argv.slice(3).join(" ");
-console.log(`Liri Command: ${input}`);
+
+heyLiri(command);
+
+function heyLiri(command) {
+  if (command === "concert-this") {
+    concertThis(query);
+  } else if (command === "spotify-this-song") {
+    spotifyThis(query);
+  } else if (command === "movie-this") {
+    movieThis(query);
+  } else if (command === "do-what-it-says") {
+    runCommand();
+  } else
+  console.log("That wasn't a choice. Go back to school.")
+}
+
+function concertThis(query) {
+
+  console.log(query);
+  axios.get("https://rest.bandsintown.com/artists/" + query + "/events?app_id=" + bandsintown.id).then(
+    function (response) {
+      response.data.forEach(concert => {
+        console.log("*-----------------------**********-------------------------*");
+        console.log(
+          concert.venue.name + "\n" + 
+          concert.venue.city + ", " + concert.venue.region + "\n" +
+          concert.datetime);
+        console.log("*-----------------------**********-------------------------*");
+      })
+    }
+  );
+  console.log();
+}
 
 
-switch (input) {
-  case "concert-this":
-    console.log(`Query: ${query}`);
-    bandsInTown(query);
-    break;
-  case "spotify-this-song":
-    if (query == "") {
-      query = "This Love";
+function spotifyThis(query) {
+
+  if (query === "") {
+    query = "Ordinary People";
+  }
+  spotify.search({ type: 'track', query: query }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
     }
-    console.log(`
-      Query: ${query}\n
-    `);
-    spotifySearch(query);
-    break;
-  case "movie-this":
-    if (query == "") {
-      query = "Crash"
+    console.log("*-----------------------**********-------------------------*");
+    console.log(
+      "Artist: " + data.tracks.items[0].artists[0].name + "\n" +
+      "Song: " + data.tracks.items[0].name + "\n" +
+      "Song: " + data.tracks.items[3].preview_url + "\n" +
+      "Song: " + data.tracks.items[0].album.name
+      )
+    console.log("*-----------------------**********-------------------------*");
+
+  });
+}
+
+
+function movieThis(query) {
+
+  if (query === "") {
+    movie = "3 Ninjas";
+  }
+
+  axios.get("http://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=" + omdbKey.id).then(
+    function (response) {
+      console.log("*-----------------------**********-------------------------*");
+      console.log(
+        "Title: " + response.data.Title + "\n" +
+        "Year: " + response.data.Year + "\n" +
+        "Country: " + response.data.Country + "\n" +
+        "Language: " + response.data.Language + "\n" +
+        "Movie Plot: " + response.data.Plot + "\n" +
+        "IMDB Rating: " + response.data.imdbRating + "\n" +
+        "Rotten Tomatoes: " + response.data.Ratings[1] + "\n" +
+        "Actors: " + response.data.Actors);
+      console.log("*-----------------------**********-------------------------*");
     }
-    console.log(`Query: ${query}`);
-    omdb(query);
-    break;
-  case "do-what-it-says":
-    readFile();
-    break;
-  default:
-    console.log(`Not a valid input.`);
-    break;
+  )
+};
+
+function runCommand() {
+
+  fs.readFile("random.txt", "utf8", function (error, data) {
+    var arr = data.split(",");
+    var command = arr[0];
+    var query = arr[1].split('"').join('');
+
+    if (error) {
+      return console.log(error);
+    }
+
+    if (command === "concert-this"){
+      concertThis(query);
+    }
+    if (command === "spotify-this-song"){
+      spotifyThis(query);
+    }
+    if (command === "movie-this"){
+      movieThis(query);
+    }
+
+  });
 }
